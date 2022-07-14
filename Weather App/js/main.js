30bff27a881c3d1f732dabb8430fabc0051cadff
Umer Ahmed <84404257+imumer16@users.noreply.github.com>
2022-07-14 16:57:16 +0500

    Update main.js

--- Weather App/js/main.js
+++ Weather App/js/main.js
@@ -8,60 +8,18 @@ function startTime() {
     s = checkTime(s);
     document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
     setTimeout(startTime, 1000);
-  }
+}
       
-  function checkTime(i) {
+function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
-  }
+}
 
-// weather custom location
+
 let Weather={
     apiKet:"7960b8508bd356d405143b43bf71d6a8",
-    fetchWeather: function(city){
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q="
-            + city
-            + "&units=metric&appid="
-            + this.apiKet 
-        ) 
-        .then((response)=>response.json())
-        .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function(data){
-        const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed }=  data.wind ;
-        console.log(name,icon,description,temp,humidity,speed);
-        document.querySelector(".city").innerText="Weather in "+ name;
-        document.querySelector(".icon").src="http://openweathermap.org/img/wn/"+icon+"@4x.png";
-        document.querySelector(".description").innerText=description;
-        document.querySelector(".temp").innerText="Temprature: "+ temp+"°C";
-        document.querySelector(".humidity").innerText="Humidity: "+ humidity+"%";
-        document.querySelector(".wind").innerText="Wind Speed: " +speed+ "km/h";
-    },
-    search: function(){
-        this.fetchWeather(document.querySelector(".input1").value);
-    }
-};
-
-document.querySelector(".button1").addEventListener("click",function(){
-    Weather.search();
-});
-
-document.querySelector(".input1").addEventListener("keyup",event => {
-    if (event.keyCode === 13) {
-        Weather.search();
-    }
-  });
-
-
-
-// weather current location
-let Weather1={
-    apiKey:"7960b8508bd356d405143b43bf71d6a8",
-    fetchWeather: function(position){
+    // weather current location
+    fetchCurrentWeather: function(position){
         const lat=position.coords.latitude;
         const lon=position.coords.longitude;
         fetch("https://api.openweathermap.org/data/2.5/weather?lat="
@@ -88,12 +46,46 @@ let Weather1={
     },
     getLocation: function () {
         if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(this.fetchWeather);
+          navigator.geolocation.getCurrentPosition(this.fetchCurrentWeather);
         } else { 
             alert("Geolocation is not supported by this browser.");
         }
+    },
+    // weather custom location
+    fetchWeather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apiKet 
+        ) 
+        .then((response)=>response.json())
+        .then((data) => {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed }=  data.wind ;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText="Weather in "+ name;
+        document.querySelector(".icon").src="http://openweathermap.org/img/wn/"+icon+"@4x.png";
+        document.querySelector(".description").innerText=description;
+        document.querySelector(".temp").innerText="Temprature: "+ temp+"°C";
+        document.querySelector(".humidity").innerText="Humidity: "+ humidity+"%";
+        document.querySelector(".wind").innerText="Wind Speed: " +speed+ "km/h";
+    });
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".input1").value);
     }
 };
 
-window.onload=Weather1.getLocation();
+//Event "Click Search button"
+document.querySelector(".button1").addEventListener("click",function(){ Weather.search(); });   
+//Event "Enter key"
+document.querySelector(".input1").addEventListener("keyup",event => {
+    if (event.keyCode === 13) { Weather.search(); }
+});
+
+// Get timestamp and location on page load
+window.onload=Weather.getLocation();
 window.onload=startTime();
